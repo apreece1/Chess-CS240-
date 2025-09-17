fd3cd7cff10f76d9656fc5b9bd383740344be01c
apreece1 <apreece1@byu.edu>
2025-09-17 15:49:18 -0600

    starting queen logic

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -246,6 +246,25 @@ public class ChessPiece {
                     }
                 }
                 break;
+
+            case QUEEN:
+                int[][] queenDirections = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,1},{1,-1},{-1,1}};
+
+                for (int[] direction : queenDirections) {
+                    int newRow = startRow + direction[0];
+                    int newCol = startCol + direction[1];
+
+                    if (newRow >= 1 && newRow <= 8 && newCol >= 1 && newCol <= 8) {
+                        ChessPosition newPosition = new ChessPosition(newRow, newCol);
+                        ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+
+                        if (pieceAtNewPosition == null || pieceAtNewPosition.getTeamColor() != this.getTeamColor()) {
+                            validMoves.add(new ChessMove(myPosition, newPosition, null));
+                        }
+                    }
+                }
+
+                break;
         }
         return validMoves;
     }
