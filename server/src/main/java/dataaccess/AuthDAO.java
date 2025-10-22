580671e7eea32f3539cea4d8fcf2bc0a1874c33c
apreece1 <apreece1@byu.edu>
2025-10-21 20:47:31 -0600

    building AuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,8 +1,12 @@
 package dataaccess;
 
 import model.AuthData;
+import java.util.Collection;
 
 public interface AuthDAO {
     void createAuth(AuthData auth) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    Collection<AuthData> listAuths() throws DataAccessException;
+    void clear() throws DataAccessException;
 }
