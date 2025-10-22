f716567af58949c6840ce8a588116dc2acdffd8a
apreece1 <apreece1@byu.edu>
2025-10-22 14:41:09 -0600

    Creating GameHandler

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -12,8 +12,10 @@ public class Main {
         var userDAO = new MemoryUserDAO();
 
         var authService = new AuthService(authDAO);
+        var userService = new UserService(userDAO, authService);
 
-        Server server = new Server();
+
+        Server server = new Server(userService, authService);
         server.run(8080);
 
         System.out.println("♕ 240 Chess Server");
---
+++ server/src/main/java/server/GameHandler.java
@@ -0,0 +1,10 @@
+package server;
+
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import service.AuthService;
+import model.GameData;
+import dataaccess.DataAccessException;
+
+public class GameHandler {
+}
