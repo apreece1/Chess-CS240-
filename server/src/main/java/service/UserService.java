6ffb926d50454d9b796666010599499cb224f7c4
apreece1 <apreece1@byu.edu>
2025-10-22 12:52:40 -0600

    User service updates

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -5,4 +5,13 @@ import model.UserData;
 import model.AuthData;
 
 public class UserService {
+
+    private final UserDAO userDAO;
+    private final AuthService authService;
+
+
+    public UserService(UserDAO userDAO, AuthService authService) {
+        this.userDAO = userDAO;
+        this.authService = authService;
+    }
 }
