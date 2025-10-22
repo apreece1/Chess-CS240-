6fab9cccbfe0996c0f3b422ce630c2761b7a49b1
apreece1 <apreece1@byu.edu>
2025-10-22 13:02:51 -0600

    game service updates

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,14 @@
+package service;
+
+import dataaccess.*;
+import model.AuthData;
+import model.GameData;
+import chess.ChessGame;
+import java.util.Collection;
+
+public class GameService {
+
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+}
