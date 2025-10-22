c0b7dcf09a269ca49c9cb86ef062fb7831b65bfb
apreece1 <apreece1@byu.edu>
2025-10-22 15:46:15 -0600

    fixing clear test

--- server/src/main/java/server/ClearHandler.java
+++ server/src/main/java/server/ClearHandler.java
@@ -7,4 +7,16 @@ import io.javalin.http.Context;
 
 
 public class ClearHandler {
+
+    private final UserService userService;
+    private final AuthService authService;
+    private final GameService gameService;
+
+    public ClearHandler(UserService userService, AuthService authService, GameService gameService) {
+        this.userService = userService;
+        this.authService = authService;
+        this.gameService = gameService;
+    }
+
+    public void clear(Context ctx)
 }
