5ba633950cefd0f0d7144438e2a8d515c0697b3a
apreece1 <apreece1@byu.edu>
2025-10-22 15:33:55 -0600

    lots of updates

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -9,9 +9,25 @@ import service.GameService;
 public class Server {
 
     private final Javalin javalin;
+    private final UserService userService;
+    private final AuthService authService;
+    private GameService gameService;
 
-    public Server(UserService userService, AuthService authService, GameService gameService) {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+    public Server(UserService userService, AuthService authService, GameService gameService, UserService userService1, AuthService authService1) {
+        this.userService = userService1;
+        this.authService = authService1;
+        this.gameService = gameService;
+        this.javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        registerEndpoints();
+    }
+
+    public Server(){
+        var authDAO = new dataaccess.MemoryAuthDAO();
+        var userDAO = new dataaccess.MemoryUserDAO();
+        var gameDAO = new dataaccess.MemoryGameDAO();
+    }
+
+    private void registerEndpoints(){
 
         UserHandler userHandler = new UserHandler(userService, authService);
         GameHandler gameHandler = new GameHandler(gameService);
