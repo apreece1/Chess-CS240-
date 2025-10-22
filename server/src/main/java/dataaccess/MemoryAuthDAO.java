1277e70e777770456ae003888dd6bf4713527b96
apreece1 <apreece1@byu.edu>
2025-10-21 20:59:58 -0600

    building up MemAuth

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -2,8 +2,37 @@ package dataaccess;
 
 import model.AuthData;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Collection;
 
 public class MemoryAuthDAO implements AuthDAO {
+
+    private final Map<String, AuthData> auths = new HashMap<>();
+
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public Collection<AuthData> listAuths() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
 }
