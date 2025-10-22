8186b8e40aed9cd4597bfd9f66e85ac5d4396ab0
apreece1 <apreece1@byu.edu>
2025-10-22 14:34:31 -0600

    updating server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,5 +1,11 @@
 import server.Server;
 
+import dataaccess.MemoryUserDAO;
+import dataaccess.MemoryAuthDAO;
+import service.UserService;
+import service.AuthService;
+
+
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
