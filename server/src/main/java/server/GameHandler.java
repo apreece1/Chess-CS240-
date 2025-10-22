e0b615ed42709fe101536b179abb05bd00a0fa47
apreece1 <apreece1@byu.edu>
2025-10-22 14:43:33 -0600

    Creating GameHandler

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -5,6 +5,18 @@ import io.javalin.http.Context;
 import service.AuthService;
 import model.GameData;
 import dataaccess.DataAccessException;
+import service.GameService;
 
 public class GameHandler {
+
+    private final GameService gameService;
+    private final AuthService authService;
+    private final Gson gson = new Gson();
+
+    public GameHandler(GameService gameService, AuthService authService) {
+        this.gameService = gameService;
+        this.authService = authService;
+    }
+
+
 }
