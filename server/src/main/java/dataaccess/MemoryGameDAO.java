27e53c114966ae36b419c8555a6150b2c3ea8de2
apreece1 <apreece1@byu.edu>
2025-10-22 12:20:06 -0600

    building MemoryGameDAO

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -7,10 +7,24 @@ import java.util.Map;
 import java.util.Collection;
 
 public class MemoryGameDAO  implements GameDAO{
+
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private int nextGameID = 1;
+
     @Override
     public int createGame(GameData game) throws DataAccessException {
-        return 0;
+        int gameID = nextGameID++;
+        var newGame = new GameData(
+                gameID,
+                game.whiteUsername(),
+                game.blackUsername(),
+                game.gameName(),
+                game.game()
+        );
+        games.put(gameID, newGame);
+        return gameID;
     }
+
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
