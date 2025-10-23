38ac0968c0fa53b9c26ac048db31d1d147cef266
apreece1 <apreece1@byu.edu>
2025-10-23 14:15:59 -0600

    writing tests

--- server/src/test/java/service/AuthServiceTests.java
+++ server/src/test/java/service/AuthServiceTests.java
@@ -11,3 +11,36 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuthServiceTests {
+    private AuthService authService;
+
+    @BeforeEach
+    void setUp() {
+        authService = new StubAuthService();
+    }
+
+    static class StubAuthService extends AuthService {
+        private final List<AuthData> auths = new ArrayList<>();
+
+    }
+
+    static class StubAuthService extends AuthService {
+        private final List<AuthData> auths = new ArrayList<>();
+
+        public StubAuthService() {
+            super(null);
+        }
+
+        @Override
+        public AuthData createAuth(String username) {
+            AuthData auth = new AuthData(username, username);
+            auths.add(auth);
+            return auth;
+        }
+
+        @Override
+        public AuthData verifyAuth(String authToken) throws DataAccessException {
+            if (authToken == null || authToken.isBlank()) {
+                throw new DataAccessException("Error: unauthorized");
+            }
+
+            
