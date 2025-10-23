15a23c34089f87b53fb6043197340fae05f6d177
apreece1 <apreece1@byu.edu>
2025-10-23 13:27:55 -0600

    writing tests

--- server/src/main/java/service/UserServiceTests.java
+++ server/src/main/java/service/UserServiceTests.java
@@ -11,10 +11,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserServiceTests {
 
     private UserService userService;
-    
+
     @BeforeEach
     void setUp() {
         MemoryUserDAO userDAO = new MemoryUserDAO();
         AuthService authService = new AuthService(new MemoryAuthDAO());
         userService = new UserService(userDAO, authService);
+    }
+
+    @Test
+    void testRegister() throws DataAccessException {
+        UserData user = new UserData("user1", "pass", "a@b.com");
+        AuthData auth = userService.register(user);
+        assertNotNull(auth);
+        assertEquals("user1", auth.username());
+    }
+
+    @Test
+    void testRegisterAlreadyTaken() throws DataAccessException {
+        UserData user = new UserData("user1", "pass", "a@b.com");
+
+        // first registration succeeds
+        userService.register(user);
+
+        // second registration should throw
+        assertThrows(DataAccessException.class, () -> userService.register(user));
     }
