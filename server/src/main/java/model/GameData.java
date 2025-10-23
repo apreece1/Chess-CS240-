0aba12f82f54e618126937c31e135657ab41e18f
apreece1 <apreece1@byu.edu>
2025-10-22 20:05:11 -0600

    slowly fixing tests

--- server/src/main/java/model/GameData.java
+++ server/src/main/java/model/GameData.java
@@ -1,26 +1,43 @@
 package model;
 
 import chess.ChessGame;
+import java.util.Objects;
 
-public record GameData {
-    int gameID;
-    String whiteUsername;
-    String blackUsername;
-    String gameName;
-    ChessGame game;
+public class GameData  {
+    private int gameID;
+    private String whiteUsername;
+    private String blackUsername;
+    private String gameName;
+    private ChessGame game;
+
 
     public GameData(int gameID, String gameName) {
         this.gameID = gameID;
         this.gameName = gameName;
     }
 
+    public GameData
+
+
     public int getGameID() { return gameID; }
     public String getGameName() { return gameName; }
     public String getWhiteUsername() { return whiteUsername; }
     public void setWhiteUsername(String whiteUsername) { this.whiteUsername = whiteUsername; }
     public String getBlackUsername() { return blackUsername; }
-    public void setBlackUsername(String blackUsername)
+    public void setBlackUsername(String blackUsername) { this.blackUsername = blackUsername; }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GameData)) return false;
+        GameData gameData = (GameData) o;
+        return gameID == gameData.gameID;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameID);
+    }
 
 }
 
