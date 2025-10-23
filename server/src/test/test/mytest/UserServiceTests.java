1637c2f299d2000a147498d9fa5c612c18815250
apreece1 <apreece1@byu.edu>
2025-10-23 12:25:23 -0600

    writing tests

--- server/src/test/test/mytest/UserServiceTests.java
+++ server/src/test/test/mytest/UserServiceTests.java
@@ -1,4 +1,19 @@
 package mytest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.UserService;
+import service.AuthService;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserServiceTests {
+
+    private UserService userService;
+    private AuthService authService;
+    private UserDAO userDAO;
+
+    @
+
 }
