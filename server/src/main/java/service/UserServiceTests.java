3c86d12ee67b4c113b294e554117e6cadd036484
apreece1 <apreece1@byu.edu>
2025-10-23 13:23:53 -0600

    writing tests

---
+++ server/src/main/java/service/UserServiceTests.java
@@ -0,0 +1,13 @@
+package service;
+
+import dataaccess.*;
+import model.UserData;
+import model.AuthData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTests {
+
+    private UserService userService;
