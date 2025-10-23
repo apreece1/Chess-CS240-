31b2bd7ae86601860430fb363bc4106a92cda132
apreece1 <apreece1@byu.edu>
2025-10-23 14:10:30 -0600

    writing tests

---
+++ server/src/test/java/service/AuthServiceTests.java
@@ -0,0 +1,13 @@
+package service;
+
+import dataaccess.DataAccessException;
+import model.AuthData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTests {
