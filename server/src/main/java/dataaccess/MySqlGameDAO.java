42b309ed5b86917948cfccf0b942e0f0076be80c
apreece1 <apreece1@byu.edu>
2025-11-11 16:13:18 -0700

    sqlauth cont

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -1,4 +1,14 @@
 package dataaccess;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dataaccess.dataaccess.DatabaseManager;
+import model.GameData;
+import model.ChessGame;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class MySqlGameDAO {
 }
