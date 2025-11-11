1aeecd6c0805faddac128545b8cca907903834a9
apreece1 <apreece1@byu.edu>
2025-11-11 15:44:41 -0700

    sqlauth

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -13,14 +13,23 @@ public class MySqlAuthDAO {
         String sql = "INSERT INTO AuthToken (authToken, username) VALUES (?, ?)";
         try (var conn = DatabaseManager.getConnection();
              var stmt = conn.prepareStatement(sql)) {
+
             stmt.setString(1, auth.authToken());
             stmt.setString(2, auth.username());
             stmt.executeUpdate();
+
         } catch (SQLException ex) {
             throw new DataAccessException("Failed to create auth token", ex);
         }
     }
 
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        String sql = "SELECT authToken, username FROM AuthToken WHERE authToken = ?";
+        try (var conn = DatabaseManager.getConnection();
+             var stmt = conn.prepareStatement(sql)) {
 
-}
+
+
+        }
 
