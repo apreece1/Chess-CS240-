c291e8bc4b97b7aa5fb4c4a858d1d752e6002661
apreece1 <apreece1@byu.edu>
2025-11-14 10:11:26 -0700

    sqluserdao

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -29,6 +29,24 @@ public class MySqlUserDAO implements UserDAO{
         } catch (SQLException e) {
             throw new DataAccessException("Unable to create user table: " + e.getMessage());
         }
-
-
     }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        String sql = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, user.username());
+            stmt.setString(2, user.password());  // later: hash with BCrypt
+            stmt.setString(3, user.email());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to insert user: " + e.getMessage());
+        }
+    }
+
+    
+}
