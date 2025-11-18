721a54fc1005415be5b554ee9699f472c7fe2a2c
apreece1 <apreece1@byu.edu>
2025-11-18 13:12:28 -0700

    severfacade

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -11,3 +11,21 @@ import java.util.List;
 import java.util.Map;
 
 public class ServerFacade {
+
+    private final String baseUrl;
+    private final Gson gson = new Gson();
+
+    public ServerFacade(int port) {
+        this.baseUrl = "http://localhost:" + port;
+    }
+
+    public AuthData register(String username, String password, String email) throws Exception {
+        var body = Map.of(
+                "username", username,
+                "password", password,
+                "email", email
+        );
+        return makeRequest("POST", "/user", body, AuthData.class, null);
+    }
+
+}
