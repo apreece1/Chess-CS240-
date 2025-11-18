994ccb0f6fa2ebad025af6db6cd9effee2cbdb35
apreece1 <apreece1@byu.edu>
2025-11-18 13:10:15 -0700

    updating user handler

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,13 @@
+package client;
+
+import com.google.gson.Gson;
+import model.AuthData;
+import model.GameData;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+public class ServerFacade {
