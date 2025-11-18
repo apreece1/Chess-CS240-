b9f80bc2f1fa02ca7d1633cd16284e72dfc44360
apreece1 <apreece1@byu.edu>
2025-11-18 16:44:07 -0700

    fixed dao test

---
+++ server/src/main/java/dataaccess/SQLGameDAOTest.java
@@ -0,0 +1,126 @@
+package dataaccess;
+
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SQLGameDAOTest {
+
+    private GameDAO gameDAO;
+
+    @BeforeEach
+    void setUp() throws DataAccessException {
+        gameDAO = new MySqlGameDAO();
+        gameDAO.clear();
+    }
+
+    @Test
+    void clear_positive_clearsAllGames() throws DataAccessException {
+        GameData game1 = new GameData(0, "Game 1", "white1", "black1", null);
+        GameData game2 = new GameData(0, "Game 2", "white2", "black2", null);
+
+        gameDAO.createGame(game1);
+        gameDAO.createGame(game2);
+
+        Collection<GameData> beforeClear = gameDAO.listGames();
+        assertEquals(2, beforeClear.size());
+
+        gameDAO.clear();
+
+        Collection<GameData> afterClear = gameDAO.listGames();
+        assertNotNull(afterClear);
+        assertTrue(afterClear.isEmpty());
+    }
+
+    @Test
+    void clear_negative_onEmptyTable_noException() {
+        assertDoesNotThrow(() -> gameDAO.clear());
+    }
+
+    @Test
+    void createGame_positive_persistsGame() throws DataAccessException {
+        GameData game = new GameData(0, "My Game", "whiteUser", "blackUser", null);
+
+        gameDAO.createGame(game);
+
+        Collection<GameData> games = gameDAO.listGames();
+        assertEquals(1, games.size());
+
+        GameData fromDb = games.iterator().next();
+        assertEquals("My Game", fromDb.getGameName());
+        assertEquals("whiteUser", fromDb.getWhiteUsername());
+        assertEquals("blackUser", fromDb.getBlackUsername());
+    }
+
+    @Test
+    void createGame_negative_nullGame_throws() {
+        assertThrows(NullPointerException.class, () -> gameDAO.createGame(null));
+    }
+
+    @Test
+    void getGame_positive_existingGame() throws DataAccessException {
+        GameData game = new GameData(0, "Game X", "whiteUser", "blackUser", null);
+        gameDAO.createGame(game);
+
+        Collection<GameData> games = gameDAO.listGames();
+        GameData stored = games.iterator().next();
+        int gameID = stored.getGameID();
+
+        GameData fromDb = gameDAO.getGame(gameID);
+        assertNotNull(fromDb);
+        assertEquals("Game X", fromDb.getGameName());
+        assertEquals("whiteUser", fromDb.getWhiteUsername());
+        assertEquals("blackUser", fromDb.getBlackUsername());
+    }
+
+    @Test
+    void getGame_negative_nonExistingGame_returnsNull() throws DataAccessException {
+        assertNull(gameDAO.getGame(999999));
+    }
+
+    @Test
+    void listGames_positive_multipleGamesReturned() throws DataAccessException {
+        gameDAO.createGame(new GameData(0, "Game 1", "w1", "b1", null));
+        gameDAO.createGame(new GameData(0, "Game 2", "w2", "b2", null));
+        gameDAO.createGame(new GameData(0, "Game 3", "w3", "b3", null));
+
+        Collection<GameData> games = gameDAO.listGames();
+        assertEquals(3, games.size());
+    }
+
+    @Test
+    void listGames_negative_emptyTable_returnsEmpty() throws DataAccessException {
+        Collection<GameData> games = gameDAO.listGames();
+        assertNotNull(games);
+        assertTrue(games.isEmpty());
+    }
+
+    @Test
+    void updateGame_positive_updatesExistingGame() throws DataAccessException {
+        GameData original = new GameData(0, "Old Name", "whiteUser", "blackUser", null);
+        gameDAO.createGame(original);
+
+        Collection<GameData> games = gameDAO.listGames();
+        GameData stored = games.iterator().next();
+        int gameID = stored.getGameID();
+
+        GameData updated = new GameData(gameID, "New Name", "newWhite", "newBlack", null);
+        gameDAO.updateGame(updated);
+
+        GameData fromDb = gameDAO.getGame(gameID);
+        assertNotNull(fromDb);
+        assertEquals("New Name", fromDb.getGameName());
+        assertEquals("newWhite", fromDb.getWhiteUsername());
+        assertEquals("newBlack", fromDb.getBlackUsername());
+    }
+
+    @Test
+    void updateGame_negative_nonExistingGame_throws() {
+        GameData fake = new GameData(999999, "Does not exist", "w", "b", null);
+        assertThrows(DataAccessException.class, () -> gameDAO.updateGame(fake));
+    }
+}
