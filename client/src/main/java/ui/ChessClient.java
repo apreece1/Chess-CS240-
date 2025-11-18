02ed5770e242dee220fad76fc968fb507d0539c5
apreece1 <apreece1@byu.edu>
2025-11-18 14:39:32 -0700

    chess client

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -19,4 +19,19 @@ public class ChessClient {
     public ChessClient(ServerFacade facade) {
         this.facade = facade;
     }
+
+    public void run() {
+        System.out.println("♕ Welcome to CS 240 Chess!");
+        boolean running = true;
+
+        while (running) {
+            try {
+                running = (state == State.PRELOGIN) ? preloginLoop() : postloginLoop();
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+        System.out.println("Goodbye!");
+    }
 }
