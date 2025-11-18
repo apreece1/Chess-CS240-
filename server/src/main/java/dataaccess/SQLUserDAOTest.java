01f575cde3938b300f41ba3de24652e07bf5e824
apreece1 <apreece1@byu.edu>
2025-11-18 16:16:19 -0700

    writing tests

---
+++ server/src/main/java/dataaccess/SQLUserDAOTest.java
@@ -0,0 +1,11 @@
+package dataaccess;
+
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SQLUserDAOTest {
+}
