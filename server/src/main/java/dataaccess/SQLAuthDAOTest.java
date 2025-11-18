362f561671160c2b182f8297c1724af8ca85b9a6
apreece1 <apreece1@byu.edu>
2025-11-18 16:28:49 -0700

    writing tests

--- server/src/main/java/dataaccess/SQLAuthDAOTest.java
+++ server/src/main/java/dataaccess/SQLAuthDAOTest.java
@@ -33,4 +33,21 @@ public class SQLAuthDAOTest {
     }
 
 
+    @Test
+    void createAuth_positive_createsUniqueToken() throws DataAccessException {
+        AuthData auth1 = authDAO.createAuth("josh");
+        AuthData auth2 = authDAO.createAuth("josh");
+
+        assertNotNull(auth1);
+        assertNotNull(auth2);
+        assertNotEquals(auth1.authToken(), auth2.authToken());
+    }
+
+    @Test
+    void createAuth_negative_nullUsername_throws() {
+        assertThrows(DataAccessException.class, () -> authDAO.createAuth(null));
+    }
+
+    
+
 }
