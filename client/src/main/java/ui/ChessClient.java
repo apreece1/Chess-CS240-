ad728d0593e254fe4311bc61a8f283a4841de0eb
apreece1 <apreece1@byu.edu>
2025-11-18 14:33:01 -0700

    cont

---
+++ client/src/main/java/ui/ChessClient.java
@@ -0,0 +1,22 @@
+package ui;
+
+import client.ServerFacade;
+import model.AuthData;
+import model.GameData;
+
+import java.util.*;
+
+public class ChessClient {
+
+    private final ServerFacade facade;
+    private AuthData currentUser;
+    private List<GameData> lastGames = new ArrayList<>();
+    private final Scanner scanner = new Scanner(System.in);
+
+    private enum State { PRELOGIN, POSTLOGIN }
+    private State state = State.PRELOGIN;
+
+    public ChessClient(ServerFacade facade) {
+        this.facade = facade;
+    }
+}
