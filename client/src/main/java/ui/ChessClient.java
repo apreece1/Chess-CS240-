b7be88c8c07c902cd89ba2e1f51d06477a18da86
apreece1 <apreece1@byu.edu>
2025-11-18 17:12:50 -0700

    updating client

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -9,3 +9,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ChessClient {
+    private final ServerFacade facade;
+    private AuthData currentUser;
+    private List<GameData> lastGames = new ArrayList<>();
+    private final Scanner scanner = new Scanner(System.in);
+
+    private enum State { PRELOGIN, POSTLOGIN }
+    private State state = State.PRELOGIN;
+
+    public ChessClient(ServerFacade facade) {
+        this.facade = facade;
+    }
+
+
+}
