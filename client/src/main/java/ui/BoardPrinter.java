9152c250bde667f9ff1809cfedf547040b14d211
apreece1 <apreece1@byu.edu>
2025-11-18 17:29:27 -0700

    client finsihed setup board.

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -37,6 +37,33 @@ public class BoardPrinter {
         System.out.println();
     }
 
+    private static void printBlackView() {
+        char[] files = {'h','g','f','e','d','c','b','a'};
+
+        for (int rank = 1; rank <= 8; rank++) {
+            System.out.print(EscapeSequences.SET_TEXT_BOLD + rank + " " + EscapeSequences.RESET_TEXT_BOLD_FAINT);
+
+            for (int f = 0; f < 8; f++) {
+                boolean light = ((rank + f) % 2 == 0);
+                String bg = light ? EscapeSequences.SET_BG_COLOR_LIGHT_GREY
+                        : EscapeSequences.SET_BG_COLOR_DARK_GREY;
+
+                char file = files[f];
+                String piece = initialPiece(rank, file);
+
+                System.out.print(bg + piece + EscapeSequences.RESET_BG_COLOR);
+            }
+            System.out.println();
+        }
+
+        System.out.print("  ");
+        for (char c : files) {
+            System.out.print(EscapeSequences.SET_TEXT_BOLD + " " + c + " " + EscapeSequences.RESET_TEXT_BOLD_FAINT);
+        }
+        System.out.println();
+    }
+
+
 
 
 
