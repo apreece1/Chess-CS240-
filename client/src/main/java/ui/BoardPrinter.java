aec99126f600b08bc52c16f93c61b10104df77c5
apreece1 <apreece1@byu.edu>
2025-11-18 17:23:43 -0700

    client finsihed setup board.

--- client/src/main/java/ui/BoardPrinter.java
+++ client/src/main/java/ui/BoardPrinter.java
@@ -1,4 +1,14 @@
 package ui;
 
 public class BoardPrinter {
-}
+
+    public static void printInitialBoard(boolean whitePerspective) {
+        System.out.println(EscapeSequences.ERASE_SCREEN);
+
+        if (whitePerspective) {
+            printWhiteView();
+        } else {
+            printBlackView();
+        }
+    }
+
