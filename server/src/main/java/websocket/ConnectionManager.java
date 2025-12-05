b6c9cbce3733e5cac8d498d924f41a9c81458252
apreece1 <apreece1@byu.edu>
2025-12-04 21:14:39 -0700

    ConnectionManager

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -13,6 +13,14 @@ public class ConnectionManager {
         WsContext ctx;
     }
 
+    private final Map<Integer, List<Connection>> connections = new ConcurrentHashMap<>();
 
-    
+    public void addConnection(int gameId, String username, WsContext ctx) {
+        var list = connections.computeIfAbsent(gameId, k -> new ArrayList<>());
+        Connection c = new Connection();
+        c.gameId = gameId;
+        c.username = username;
+        c.
+
+
 }
