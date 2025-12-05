b2b3c46095008d4cbe58a54a93bedcb773d60a99
apreece1 <apreece1@byu.edu>
2025-12-04 20:56:31 -0700

    Websocket Handler

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -6,5 +6,16 @@ import jakarta.websocket.server.ServerEndpoint;
 import websocket.commands.UserGameCommand;
 
 
+@ServerEndpoint("/ws")
 public class WebSocketHandler {
+
+    private static final Gson gson = new Gson();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("[WS OPEN] " + session.getId());
+    }
+
+
 }
+
