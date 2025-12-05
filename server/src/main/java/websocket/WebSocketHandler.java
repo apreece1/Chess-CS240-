b77baea4b40c2b2aa93b70091ae082494caa6cf4
apreece1 <apreece1@byu.edu>
2025-12-04 20:55:59 -0700

    Websocket Handler

---
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -0,0 +1,10 @@
+package websocket;
+
+import com.google.gson.Gson;
+import jakarta.websocket.*;
+import jakarta.websocket.server.ServerEndpoint;
+import websocket.commands.UserGameCommand;
+
+
+public class WebSocketHandler {
+}
