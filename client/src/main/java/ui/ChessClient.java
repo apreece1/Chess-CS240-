a8cdc305d5ad742b1a8b6e0d632321065dec4b14
apreece1 <apreece1@byu.edu>
2025-12-09 16:23:34 -0700

    updating chess client for highlights

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -11,3 +11,22 @@ import model.GameData;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+public class ChessClient implements GameplayObserver {
+    private final ServerFacade facade;
+    private final int port;
+    private WebSocketFacade ws;
+
+    private AuthData currentUser;
+    private List<GameData> lastGames = new ArrayList<>();
+    private GameData currentGame;
+    private final Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void onLoadGame(GameData game) {
+        System.out.println("[CLIENT] LOAD_GAME received for gameID=" + game.getGameID());
+        this.currentGame = game;
+        redrawBoard();
+    }
+
+    
