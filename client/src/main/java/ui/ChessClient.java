bffdda2803b92013f54951e0824b5577a8a8dd1a
apreece1 <apreece1@byu.edu>
2025-12-09 16:48:31 -0700

    updating chess client for highlights

--- client/src/main/java/ui/ChessClient.java
+++ client/src/main/java/ui/ChessClient.java
@@ -233,6 +233,31 @@ public class ChessClient implements GameplayObserver {
         }
     }
 
+    private void printGameHelp() {
+        System.out.println("""
+                Gameplay commands:
+                  help      - Show this help
+                  redraw    - Redraw the chess board
+                  move      - Make a move
+                  highlight - Highlight legal moves for a piece
+                  resign    - Resign the game
+                  leave     - Leave the game and return to post-login menu
+                """);
+    }
+
+    private void redrawBoard() {
+        if (currentGame == null) {
+            System.out.println("No game loaded.");
+            return;
+        }
+        boolean whiteOnBottom = true;
+        if (currentUser != null &&
+                currentGame.getBlackUsername() != null &&
+                currentUser.username().equals(currentGame.getBlackUsername())) {
+            whiteOnBottom = false;
+        }
+
+
 
 
 
