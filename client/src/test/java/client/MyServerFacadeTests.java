eba90902edbfa7a52c95b3213cc17d5188254837
apreece1 <apreece1@byu.edu>
2025-12-09 22:41:40 -0700

    doing tests for stage 5

--- client/src/test/java/client/MyServerFacadeTests.java
+++ client/src/test/java/client/MyServerFacadeTests.java
@@ -11,3 +11,20 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyServerFacadeTests {
+    private static Server server;
+    private static ServerFacade facade;
+
+    @BeforeAll
+    public static void startServer() {
+        server = new Server();
+        int port = server.run(0);
+        facade = new ServerFacade(port);
+    }
+
+    @AfterAll
+    public static void stopServer() {
+        if (server != null) {
+            server.stop();
+        }
+    }
+}
