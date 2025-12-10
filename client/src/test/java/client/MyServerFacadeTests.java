5ad10665090e4ea6a1872b614044eb3c81ad99ea
apreece1 <apreece1@byu.edu>
2025-12-09 22:39:57 -0700

    doing tests for stage 5

---
+++ client/src/test/java/client/MyServerFacadeTests.java
@@ -0,0 +1,13 @@
+package client;
+
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.*;
+
+import server.Server;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyServerFacadeTests {
